b6eeaee144620dab78cb9ececd07d4b9c5230a10
Buena Chan <buenachan@buenas-mbp.home>
2018-06-03 22:21:56 -0400

    added react components Main

---
+++ client/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+
+export default class Main extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <Navbar />
+    )
+  }
+}
